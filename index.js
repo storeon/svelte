88a9e60bb0672fd85f9267dee0b7bb92458f1f6a
distolma <djmakaron2009@gmail.com>
2019-05-17 23:29:17 +0300

    improve jsdoc

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 var createStore = require('storeon')
 
 /**
+ * Initialize new store and apply all modules to the store for Svelte app.
  *
- * @param {*} modules
+ * @param {modules[]} modules Functions which will set initial state
+ *                            define reducer and subscribe
+ *                            to all system events
  *
- * @return {}
+ * @return {connect} The store connector.
+ *
+ * @example
+ * import { createSvelteStore } from "storeon-svelte";
+ *
+ * let counter = store => {
+ *  store.on("@init", () => ({ count: 0 }));
+ *  store.on("inc", ({ count }) => ({ count: count + 1 }));
+ * };
+ * export const connect = createSvelteStore([counter]);
  */
 function createSvelteStore (modules) {
   var store = createStore(modules)
 
   /**
+   * Hook-like function to use Storeon in Svelte app
+   *
+   * @param {string} key Key of state field
    *
    */
   return function (key) {
     var state = store.get()
     var subscribers = []
 
+    /**
+     * Subscription for the state
+     *
+     * @param {function} run Callback function
+     *
+     */
     function subscribe (run) {
       subscribers.push(run)
       run(state[key])
