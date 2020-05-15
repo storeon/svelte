c303f8130511a5222915bdc88cf771e59fbc0d0c
Mayke <mayke_sargento@hotmail.com>
2020-05-15 19:24:06 -0300

    fixing subcription for boolean values

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function useStoreon (...keys) {
 
   store.on('@changed', (_, changed) => {
     keys.forEach(key => {
-      if (changed[key] && subscribers[key]) {
+      if (key in changed && subscribers[key]) {
         subscribers[key](changed[key])
       }
     })
